b592ac5811784ed37e2e7a429f52c54601a99716
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-02-21 10:47:54 +0200

    commit

--- client.js
+++ client.js
@@ -1,4 +1,14 @@
 import {network} from "./network.js"
+var output='';
+var xhttp=new FXMLHttpRequest();
+xhttp.onreadystatechange=function(){
+     if(this.readState=4&&this.status==200){
+        document.getElementById("meetings").innerHTML=xhttp.responseText;
+     }
+}
+xhttp.open("GET","./server", true);
+xhttp.send();
+
 
 
 
--- network.js
+++ network.js
@@ -7,3 +7,23 @@ export function sendAsync(req,callback){
         callback(res);
     }, 3000)
 }
+
+export function send(req){
+    const _server=new server();
+    const res=_server.hendleRequest(req);
+    downloading();
+    return res;
+}
+function delay(ms){
+    var start=Date.now(),
+    now=start;
+    while(now-start<ms){
+        now=Date.now();
+    }
+}
+
+function downloading(during=3000){
+    console.log("downloading...");
+    delay(during);
+    console.log("finished downloading");
+}
