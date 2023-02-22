8396fbb7536474dff7c9281f8cb5b2bd77b6d868
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-02-22 20:19:47 +0200

    commit

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-import {dataBse} from "./dataBase.js"
+import {dataBase} from "./dataBase.js"
+export function hendleRequest(req){
+    var res;
+    if(req.method=='GET'){
+       res= dataBase.get(req.url);
+    }
+    if(req.method=='POST'){
+        dataBase.post(req.url,req.body);
+    }
+    return res;
+}
